c519cd9d1e30a565fae3b3964ec11a2167f77183
besupernow <besupernowmchs@outlook.com>
2024-11-10 22:59:03 -0500

    Update navigation and styling; replace logo and buttons, enhance color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,18 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        sailHudson: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          "america":"#3b82f6",
+        },
+      },
+    ],
+  },
 }
